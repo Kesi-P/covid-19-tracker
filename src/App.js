98d0e2b007af727bb44c56c639aed1bd41f21162
Kesi-P <54447709+Kesi-P@users.noreply.github.com>
2021-04-07 17:55:41 +0200

    Fetch data from api using axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import {Cards,Chart,CountryPicker} from './Components'
+import Style from './App.module.css'
+import {fetchData} from './api'
+export default class App extends Component {
+    async componentDidMount(){
+        const data= await fetchData()
+        console.log(data)
+    }
+    render() {
+        return (
+            <div className={Style.container}>
+                <Cards/>
+                <Chart/>
+                <CountryPicker/>
+            </div>
+        )
+    }
 }
-
-export default App;
